3fd43562a0664cf7643cf93f8b6cd685db91a1f5
kmisiarz <kmisiarz@UGent.be>
2025-10-28 14:56:06 +0100

    Remove unused stuff

--- backend/apis/base.py
+++ backend/apis/base.py
@@ -5,11 +5,7 @@ from backend.apis.v1 import (
     route_users,
 )
 
-# from webapps.working_hours import route_working_hours
-
 api_router = APIRouter()
 api_router.include_router(route_users.router, prefix="/users", tags=["users"])
-# api_router.include_router(
-#     route_working_hours.router, prefix="/working_hours", tags=["working_hours"]
-# )
+
 api_router.include_router(route_login.router, prefix="/login", tags=["login"])
