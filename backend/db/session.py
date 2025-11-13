1f65da1660de6d083466a12dd4be928adf983dca
kmisiarz <forkarolm@gmail.com>
2025-11-13 13:40:59 +0100

    add url schele

--- backend/db/session.py
+++ backend/db/session.py
@@ -2,12 +2,22 @@ from typing import Generator
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from urllib.parse import urlparse
 
 from backend.core.config import settings
 
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+
+url = urlparse(SQLALCHEMY_DATABASE_URL)
+
 # when using pgadmin
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={}
+    if url.scheme.startswith("postgres")
+    else {"check_same_thread": False},
+)
 
 # when using file
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
