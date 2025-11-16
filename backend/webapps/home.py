b0ac50caba88c48418faee0811298c99959c8338
kmisiarz <forkarolm@gmail.com>
2025-11-16 14:09:00 +0100

    Add health check

--- backend/webapps/home.py
+++ backend/webapps/home.py
@@ -32,3 +32,8 @@ async def home(
             "running_games": running_games,
         },
     )
+
+@router.get("/health-check", status_code=200)
+async def health_check():
+    # This function must return successfully and not use any dependencies.
+    return {"status": "ok", "app": "running"}
